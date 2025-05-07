4431a5a6a70c31934417c499f2f04765f8fd28ba
leeversion0404 <restychan965@gmail.com>
2025-05-07 14:53:35 +0700

    Update register.go

--- register.go
+++ register.go
@@ -15,9 +15,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/shillbie/register/LineThrift"
-	"github.com/shillbie/register/helper"
-	"github.com/shillbie/register/thrift"
+	"github.com/leeversion0404/register/LineThrift"
+	"github.com/leeversion0404/register/helper"
+	"github.com/leeversion0404/register/thrift"
 
 	"github.com/ericlagergren/siv"
 	"github.com/google/uuid"
